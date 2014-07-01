712aa9a534e5362dfdd6c7da994921fd354f663f
Oli Treadwell <olitreadwell@gmail.com>
2014-07-01 08:45:09 -0500

    feature: setup key bindings

--- html5-game-tutorial-make-a-snake-game-using-html5-canvas-jquery/application.js
+++ html5-game-tutorial-make-a-snake-game-using-html5-canvas-jquery/application.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 
     // Lets save the cell width in a variable for easy control
     var cw = 10;
+    var d = "right"; //default direction
 
     // Lets paint the canvas now
     ctx.fillStyle = "white";
@@ -34,6 +35,11 @@ $(document).ready(function(){
     function paint()
     {
         // To avoid the snake trail we need to pain the BG on every frame
+        // Lets pain the canvas now
+        ctx.fillStyle = "white";
+        ctx.fillRect(0,0, w, h);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(0,0,w,h);
 
         // The movement code for the snake to come here.
         // The logic is simple
@@ -42,7 +48,12 @@ $(document).ready(function(){
         var ny = snakeArray[0].y;
         // These were the position of the head cell.
         // We will increment it to get the new head position
-        nx++;
+        // Lets add proper direction based movement now
+        if (d == "right") nx++;
+        else if (d == "left") nx--;
+        else if (d == "up") ny--;
+        else if (d == "down") ny++;
+
 
         var tail = snakeArray.pop(); //pops out the last cell
         tail.x = nx
@@ -58,6 +69,20 @@ $(document).ready(function(){
             ctx.strokeRect(c.x*cw, c.y*cw, cw, cw);
         }
     }
+
+    // Lets add the keyboard controls now
+
+    $(document).keyDown(function(e){
+        var key = e.which;
+        // We will add another clause to prevent reverse gear
+        if (key == "37" && d != "right") d ="left";
+        else if (key == "38" && d != "down") d ="up";
+        else if (key == "39" && d != "left") d ="right";
+        else if (key == "40" && d != "up") d ="down";
+    })
+
+
+
     // Lets move the snake now using a timer which will trigger the pain function
     // every 60ms
     game_loop = setInterval(paint, 60);
